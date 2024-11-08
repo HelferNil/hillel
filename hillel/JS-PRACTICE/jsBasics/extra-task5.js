f0a2d1dfc3260e5e95db67cd10e460d32aee2c10
Serhii Kostiuk <helfernil@gmail.com>
2024-11-08 16:33:20 +0200

    Homework 5

---
+++ hillel/JS-PRACTICE/jsBasics/extra-task5.js
@@ -0,0 +1,5 @@
+let string1 = "JumPiNg StRiNg";
+console.log(string1 == string1.toUpperCase());
+
+string1 = "ONLYUPPERCASE STRING1";
+console.log(string1 == string1.toUpperCase());
---
+++ hillel/JS-PRACTICE/jsBasics/task5_3.js
@@ -0,0 +1,12 @@
+const number = 4;
+console.log("Цикл for");
+for (let i=1; i<11; i++) {
+    console.log(i + ' * ' + number + ' = ' + i*number);
+}
+
+console.log("Цикл while");
+i=1;
+while (i<11) {
+    console.log(i + ' * ' + number + ' = ' + i*number);
+    i++;
+}
